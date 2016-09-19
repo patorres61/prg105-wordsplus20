c86206103595908bd5b9f9a85f310d5fa3e2205d
Phyllis Torres <ptorres@students.mchenry.edu>
2016-09-19 17:17:56 -0500

    Add files via upload

--- words20char.py
+++ words20char.py
@@ -4,17 +4,20 @@
 length = 0
 count = 1
 word20 = 0
+totalWords = 0
 
 # Define input file
 fin = open('words.txt')
 
 # Read input file
 fin.readline()
+totalWords += 1
 
 print ('Words Greater Than 20 characters Omitting Blank Spaces\n')
 # Read file, check character count in each line
 for line in fin:
     word = line.strip()     # line.strip will strip out any blank spaces
+    totalWords += 1         # increment count for total words in file
     length = len(word)      # get length of word
     if length > 20:
         print word
@@ -22,6 +25,6 @@ for line in fin:
     else:
         count += 1
 
-print ('\nTotal number of words in file: ' + str(word20+count))
-print ('\nTotal number of words in file greater than 20 characters omitting blank spaces: ' + str(word20))
-print ('\nTotal number of words in file less than or equal to 20 characters omitting blank spaces: ' + str(count))
+print ('\nTotal number of words in file: ' + str(totalWords))
+print ('\nTotal words in file greater than 20 characters omitting blank spaces: ' + str(word20))
+print ('\nTotal words in file less than or equal to 20 characters omitting blank spaces: ' + str(count))
